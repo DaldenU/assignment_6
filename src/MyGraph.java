3ce089be9576865faf59ca771b352f21017a55ed
DaldenU <daldenuteryag@gmail.com>
2023-05-30 20:58:31 +0600

    test and fix

--- src/MyGraph.java
+++ src/MyGraph.java
@@ -26,7 +26,7 @@ public class MyGraph {
 
     public void printEdge(){
         for(int i = 0; i < numOfVertices; i++){
-            System.out.println("Vertex" + i +" connected to: ");
+            System.out.print("Vertex number " + i +" is connected to: ");
             for(int neighbor : adgList[i]){
                 System.out.println(neighbor + " ");
             }
