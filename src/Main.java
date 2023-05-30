724a58354d6149b3881badd59e003ece8618551b
DaldenU <daldenuteryag@gmail.com>
2023-05-30 20:59:44 +0600

    test and fix (forgot to commit main in the previous one)

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+
+        MyGraph myGraph = new MyGraph(4);
+        myGraph.addEdge(0, 1);
+        myGraph.addEdge(2, 3);
+        myGraph.printEdge();
+    }
+}
