fdb22cd0943427674786cf0a0b70d425f56c6fb9
DaldenU <daldenuteryag@gmail.com>
2023-05-31 15:16:14 +0600

    commentary for MyGraph

--- src/MyGraph.java
+++ src/MyGraph.java
@@ -3,6 +3,8 @@ import java.util.LinkedList;
 public class MyGraph {
     private int numOfVertices;
     private LinkedList<Integer>[] adgList;
+
+    // Constructs a MyGraph object with the specified number of vertices.
     public MyGraph(int numOfVertices){
         this.numOfVertices = numOfVertices;
         adgList = new LinkedList[numOfVertices];
@@ -11,6 +13,7 @@ public class MyGraph {
         }
     }
 
+    // Adds an edge between the source and destination vertices.
     public void addEdge(int source, int destination){
         validateVertex(source);
         validateVertex(destination);
@@ -18,12 +21,14 @@ public class MyGraph {
         adgList[destination].add(source);
     }
 
+    // Validates the given vertex index.
     private void validateVertex(int index){
         if(index < 0 || index > numOfVertices){
             throw new IllegalArgumentException("Vertex "+ index + " is out of range");
         }
     }
 
+    // Prints the edges of each vertex in the graph.
     public void printEdge(){
         for(int i = 0; i < numOfVertices; i++){
             System.out.print("Vertex number " + i +" is connected to: ");
@@ -34,6 +39,7 @@ public class MyGraph {
         }
     }
 
+    // Removes the edge between the source and destination vertices.
     public void removeEdge(int source, int destination){
         validateVertex(source);
         validateVertex(destination);
@@ -41,22 +47,27 @@ public class MyGraph {
         adgList[destination].remove(source);
     }
 
-    public boolean HaveEdge(int source, int destination){
+    // Checks if there is an edge between the source and destination vertices.
+    public boolean haveEdge(int source, int destination){
         validateVertex(source);
         validateVertex(destination);
         return adgList[source].contains(destination);
     }
 
+    // Returns the neighbors of the specified vertex.
     public LinkedList<Integer> getNeighbor(int vertex){
         validateVertex(vertex);
         return adgList[vertex];
     }
 
+    // Performs Depth-First Search starting from the specified startVertex.
     public void DFS(int startVertex){
         validateVertex(startVertex);
         boolean[] visited = new boolean[numOfVertices];
         DFSHelper(startVertex, visited);
     }
+
+    // Helper method for Depth-First Search traversal.
     private void DFSHelper(int vertex, boolean[] visited){
         visited[vertex] = true;
         System.out.println(vertex + " ");
