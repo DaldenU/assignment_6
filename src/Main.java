16aa360b1682bce5e3e762ed2692806ef6ff82ee
DaldenU <daldenuteryag@gmail.com>
2023-05-31 15:23:53 +0600

    Main class

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,6 @@
+import java.util.List;
+import java.util.Map;
+
 public class Main {
     public static void main(String[] args) {
 
@@ -5,7 +8,36 @@ public class Main {
         myGraph.addEdge(0, 1);
         myGraph.addEdge(2, 3);
         myGraph.printEdge();
-        System.out.println(myGraph.HaveEdge(0,1));
+        System.out.println(myGraph.haveEdge(0,1));
         System.out.println(myGraph.getNeighbor(2));
+
+        myGraph.DFS(2);
+        WeightedGraph<String> graph = new WeightedGraph<>();
+        Vertex<String> vertexA = new Vertex<>("A");
+        Vertex<String> vertexB = new Vertex<>("B");
+        Vertex<String> vertexC = new Vertex<>("C");
+
+        graph.addEdge(vertexA, vertexB, 1);
+        graph.addEdge(vertexA, vertexC, 2);
+        graph.addEdge(vertexB, vertexC, 3);
+
+        DijkstraSearch<String> dijkstraSearch = new DijkstraSearch<>(graph);
+
+        // Find the shortest paths from vertex A to all other vertices
+        Vertex<String> source = vertexA;
+        Map<Vertex<String>, Double> distances = dijkstraSearch.shortestPaths(source);
+        System.out.println("Shortest paths from " + source + ":");
+        for (Vertex<String> vertex : graph.getVertices()) {
+            double distance = distances.get(vertex);
+            System.out.println("To " + vertex + ": " + distance);
+        }
+
+        // Find the shortest path from vertex A to vertex C
+        List<Vertex<String>> shortestPath = dijkstraSearch.shortestPathTo(source, vertexC);
+        System.out.println("Shortest path from " + source + " to " + vertexC + ":");
+        for (Vertex<String> vertex : shortestPath) {
+            System.out.print(vertex + " ");
+        }
     }
+
 }
